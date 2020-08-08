c2eaa400d9ff48ebda1b84e1d45416e64a82fd26
maumainsaw <maumainsaw@gmail.com>
2020-08-08 11:22:09 -0500

    Cargar condicionalmente un componente dependiendo si se introduce el presupuesto

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,7 +1,7 @@
 import React,{Fragment,useState} from 'react';
 import Error from './Error';
 
-const Pregunta = () => {
+const Pregunta = ({guardarPresupuesto,guardarRestante,actualizarPregunta}) => {
     //definir state
     const [cantidad,guardarCantidad]=useState(0);
     const [error,guardarError]=useState(false);
@@ -20,6 +20,9 @@ const Pregunta = () => {
         }
         //si se pasa la validacion
         guardarError(false)
+        guardarPresupuesto(cantidad)
+        guardarRestante(cantidad)
+        actualizarPregunta(false)
 
     }
 
